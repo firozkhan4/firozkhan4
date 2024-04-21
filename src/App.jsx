8880b6e5929b37d74c6de87d595effc78004096b
firozkhan4 <firozkhan907979@gmail.com>
2024-04-21 23:21:14 +0530

    new commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Navbar from "./components/Navbar"
+import HeroSection from "./components/HeroSection"
+import ProjectSection from "./components/ProjectSection"
+
+function App() {
+
+  return (
+    <>
+
+      <Navbar />
+
+      <HeroSection />
+      <ProjectSection />
+
+    </>
+  )
+}
+
+export default App
