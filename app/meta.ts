c42a36c45914997750c7462e38d53baaeebbcb01
Firoz Khan <firozkhan907979@gmail.com>
2026-02-06 18:46:46 +0530

    fix: add s3 resume link

--- app/meta.ts
+++ app/meta.ts
@@ -2,5 +2,5 @@ export const socialLink = {
   linkedin: "https://linkedin.com/in/firozkhan4",
   github: "https://github.com/firozkhan4",
   twitter: "https://x.com/firozkhan_4",
-  resume: "https://drive.google.com/file/d/1-H8qAdF3GjT614mll0AokmqtffQr__xr/view?usp=drive_link"
+  resume: "https://firozkhan4.s3.ap-south-1.amazonaws.com/Resume"
 }
