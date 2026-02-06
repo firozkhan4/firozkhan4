606612183ea9229f12a8088603abebdafcc39866
Firoz Khan <firozkhan907979@gmail.com>
2026-02-06 19:08:40 +0530

    fix: resume link

--- app/meta.ts
+++ app/meta.ts
@@ -2,5 +2,5 @@ export const socialLink = {
   linkedin: "https://linkedin.com/in/firozkhan4",
   github: "https://github.com/firozkhan4",
   twitter: "https://x.com/firozkhan_4",
-  resume: "https://firozkhan4.s3.ap-south-1.amazonaws.com/Resume"
+  resume: "https://firozkhan4.s3.ap-south-1.amazonaws.com/Resume.pdf"
 }
